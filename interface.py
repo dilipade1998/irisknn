4eb980f7649d827c15e9e6786a0a9c6e18279d12
dilip ade <dilipade1998@gmail.com>
2023-02-05 23:13:58 +0530

    modified files

--- interface.py
+++ interface.py
@@ -9,20 +9,24 @@ app=Flask(__name__)
 @app.route("/") #home API
 
 def base():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route("/predict",methods=["POST"])
 
-def home():
-    SepalLengthCm = request.form["SepalLengthCm"]
-    SepalWidthCm = request.form["SepalWidthCm"]
-    PetalLengthCm = request.form["PetalLengthCm"]
-    PetalWidthCm =request.form["PetalWidthCm"]
+def result():
+    SepalLengthCm = request.form["Sepal Length"]
+    SepalWidthCm = request.form["Sepal Width"]
+    PetalLengthCm = request.form["Petal Length"]
+    PetalWidthCm =request.form["Petal Width"]
 
     x=iris_prediction(SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm)
     final=x.predict_species()
-
-    return render_template("next.html",data=final)
+    if final == "Iris-virginica":
+        return render_template("virginica.html",data=final)
+    elif final == "Iris-setosa":
+        return render_template("setosa.html",data=final)
+    else:
+        return render_template("versicolor.html",data=final)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=config.PORT_NUMBER,debug=True)
